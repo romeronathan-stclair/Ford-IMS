a7a2bd35505c354363e0d1db446a8c7fdf0205e2
nathan <nathan.romero01@stclairconnect.ca>
2023-02-26 13:43:25 -0500

    #1 Connect project to mongoDB

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,11 +3,14 @@ import http, { Server as HttpServer } from "http";
 import env from './utils/env';
 import session, { SessionOptions, CookieOptions } from "express-session";
 import expressSession from 'express-session';
+import { connectMongoDB } from "./config/mongoose";
+
 
 const router: Router = express.Router();
 
-
 const app = express();
+
+connectMongoDB(env.db.fullUrl);
 
 app.use(expressSession);
 app.use(`/${env.app.prefix}`, router);
---
+++ api/src/config/mongoose.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import logger from "../utils/logger";
+
+export const connectMongoDB = (url: string) => {
+    logger.info(url);
+    mongoose
+        .connect(url)
+        .then(() => {
+            logger.info("MongoDB running");
+        })
+        .catch((err) => {
+
+            logger.info(`MongoDB connection error. Please make sure MongoDB is running. ${err} URL : ${url}`);
+        });
+};
