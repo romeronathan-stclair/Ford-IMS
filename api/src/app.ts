567c5ab6d507c6d35543f50a24984418a3321e9b
nathan <nathan.romero01@stclairconnect.ca>
2023-02-26 14:30:16 -0500

    #49 Setup express cookie store

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,15 +1,33 @@
-import express, { Router } from 'express';
+import express, { RequestHandler, Router } from 'express';
 import http, { Server as HttpServer } from "http";
 import env from './utils/env';
 import session, { SessionOptions, CookieOptions } from "express-session";
-import expressSession from 'express-session';
 import { connectMongoDB } from "./config/mongoose";
-
+import MongoStore from "connect-mongo";
 
 const router: Router = express.Router();
 
 const app = express();
 
+const cookieOptions: CookieOptions = {
+    maxAge: 1000 * 60 * 10,
+    sameSite: env.isProd ? "none" : "lax",
+    secure: env.isProd ? true : false,
+};
+
+const sessionOptions: SessionOptions = {
+    secret: env.app.sessionSecret,
+    resave: true,
+    rolling: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongoUrl: env.db.fullUrl,
+
+    }),
+    cookie: cookieOptions,
+
+};
+const expressSession: RequestHandler = session(sessionOptions);
 connectMongoDB(env.db.fullUrl);
 
 app.use(expressSession);
