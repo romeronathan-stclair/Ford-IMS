5a22ecc0c2e8f93ad5123c10fdb19d55d67672c1
Nathan Romero <nathan.romero01@stclairconnect.ca>
2023-03-04 13:31:16 -0500

    Fix check if product exists

--- api/src/controllers/productDunnage.ts
+++ api/src/controllers/productDunnage.ts
@@ -31,22 +31,20 @@ export const createProductDunnage = async (req: Request, res: Response) => {
     await check("dunnageId", "dunnageId is not valid")
 
 
+
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         return res.status(500).json(errors);
     }
 
-
-
     const user: UserDocument = req.user as UserDocument;
     const productDunnage: ProductDunnageDocument = (await ProductDunnage.findOne({
         productId: req.body.productId,
-        dunnageId: req.body.dunnageId,
         isDeleted: false,
     })) as ProductDunnageDocument;
 
     if (productDunnage) {
-        return res.status(500).json("ProductDunnage already exists");
+        return res.status(500).json("ProductDunnage already exists for this product");
     }
 
     const newProductDunnage = new ProductDunnage({
